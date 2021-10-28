bd0157fe06b735ba8e69540d85b52b5e0e9270a4
kaw393939 <keith@webizly.com>
2021-10-28 17:16:46 -0400

    Started Refactor of Calculator

---
+++ calculator/calculator.py
@@ -0,0 +1,17 @@
+""" This is the increment function"""
+class Calculator:
+    """ This is the Calculator class"""
+    history = []
+
+    @staticmethod
+    def add_number(value_a, value_b):
+        """ adds number to result"""
+        return value_a + value_b
+    @staticmethod
+    def subtract_number(value_a, value_b):
+        """ subtract number from result"""
+        return value_a - value_b
+    @staticmethod
+    def multiply_numbers(value_a, value_b):
+        """ multiply two numbers and store the result"""
+        return value_a * value_b
--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,32 +1,14 @@
 """Testing the Calculator"""
-from calculator.main import Calculator
-
-def test_calculator_result():
-    """testing calculator result is 0"""
-    calc = Calculator()
-    assert calc.result == 0
+from calculator.calculator import Calculator
 
 def test_calculator_add():
     """Testing the Add function of the calculator"""
-    #Arrange by instantiating the calc class
-    calc = Calculator()
-    #Act by calling the method to be tested
-    calc.add_number(4)
-    #Assert that the results are correct
-    assert calc.result == 4
-
-def test_calculator_get_result():
-    """Testing the Get result method of the calculator"""
-    calc = Calculator()
-    assert calc.get_result() == 0
+    assert Calculator.add_number(1,2) == 3
 
 def test_calculator_subtract():
     """Testing the subtract method of the calculator"""
-    calc = Calculator()
-    calc.subtract_number(1)
-    assert calc.get_result() == -1
+    assert Calculator.subtract_number(1, 2) == -1
+
 def test_calculator_multiply():
     """ tests multiplication of two numbers"""
-    calc = Calculator()
-    result  = calc.multiply_numbers(1,2)
-    assert result == 2
+    assert Calculator.multiply_numbers(1,2) == 2
