2cdd22d3edf0f96fe199c79b98bc2ea31e8e9c8d
kaw393939 <keith@webizly.com>
2021-11-03 16:04:53 -0400

    Added the fixture for clearing the history before each test

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,25 +1,28 @@
 """Testing the Calculator"""
 import pprint
 
+import pytest
+
 from calc.calculator import Calculator
 
-def test_calculator_add_static():
+#this is how you define a function that will run each time you pass it to a test, it is called a fixture
+@pytest.fixture
+def clear_history():
+    Calculator.clear_history()
+#You have to add the fixture function as a parameter to the test that you want to use it with
+def test_calculator_add_static(clear_history):
     """testing that our calculator has a static method for addition"""
-    assert Calculator.add_numbers(1.0,2.0) == 3.0
+    assert Calculator.add_numbers(1.0) == 1.0
 
-def test_calculator_subtract():
+def test_calculator_subtract_static(clear_history):
     """Testing the subtract method of the calc"""
     assert Calculator.subtract_numbers(1.0,2.0) == -3.0
 
-def test_calculator_multiply():
+def test_calculator_multiply_static(clear_history):
     """Testing the subtract method of the calc"""
     assert Calculator.multiply_numbers(1.0,2.0) == 2.0
 
-def test_calculator_history_static_property():
+def test_calculator_history_static_property(clear_history):
     """Testing the subtract method of the calc"""
     Calculator.add_numbers(1.0,2.0)
-    assert len(Calculator.history) == 3
-
-def test_calculator_history_getAddtionCalculation():
-    calculation = Calculator.history[0]
-    assert calculation.getResult()  == 3.0
+    assert len(Calculator.history) == 1
